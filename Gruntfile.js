3e9f9e94be031d2d76058095c394b4ce81c4cf00
Hack Reactor Students <accounts@hackreactor.com>
2017-03-09 15:10:09 -0800

    change spec files to run

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,7 +99,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
-      // add your production server task here
+      //asdf
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
