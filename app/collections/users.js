6c735b33ce6248597a436f982472f8c989f92edb
Hack Reactor Students <accounts@hackreactor.com>
2017-03-08 17:29:42 -0800

    stuff

--- app/collections/users.js
+++ app/collections/users.js
@@ -1,6 +1,6 @@
 // NOTE: this file is not needed when using MongoDB
 var inTents = 'INTENSE!!!?!';
-console.log(inTents);
+console.log('tents', inTents);
 
 var db = require('../config');
 var User = require('../models/user');
